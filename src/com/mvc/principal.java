f80a60e533c46f8b3b8ffcd6dd7d7fdb59679c45
Nataly Moros <nataly.moros1990@gmail.com>
2011-07-13 15:52:23 -0430

    otra vez commit

--- src/com/mvc/principal.java
+++ src/com/mvc/principal.java
@@ -8,6 +8,7 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("=)");
+		System.out.println("Esto es divertido");
 
 	}
 
